bada684c89544d15370d81a466b5135586cfe910
Breckin1027 <brluke01@wsc.edu>
2025-11-11 13:44:57 -0600

    Completed main functionality of my stored procedure to create a new course

--- v3Files/breckin_procedures.sql
+++ v3Files/breckin_procedures.sql
@@ -1,29 +1,30 @@
 /*
-Procedure Ideas:
-- Create a new user (Student, Faculty)
-- Current passing students
-- Create a new course 
-- Remove old courses
-*/
-
-DROP PROCEDURE IF EXISTS p_create_course()
+Add data validation to this procedure 
 
 DELIMITER //
 
-CREATE PROCEDURE create_course
+CREATE OR REPLACE PROCEDURE sp_insert_course_if_valid
 (
 	IN course_name_param VARCHAR(255),
     IN course_description_param TEXT,
+    IN course_active_param TINYINT(4),
+    IN created_userid_param INT(11),
     OUT course_id_param INT(11)
 )
 BEGIN
 	DECLARE date_created TIMESTAMP;
-    DECLARE course_active TINYINT(4);
     
+    SET course_id_param = NULL;
+    
+    START TRANSACTION; 
+    
+	INSERT INTO course(name, description, active, created_userid)
+    VALUES (course_name_param, course_description_param, course_active_param, created_userid_param);
+	
+    SET course_id_param = LAST_INSERT_ID();
+    COMMIT;
 END//
 DELIMITER ;
-    
-	/*
-    First Step: Validate the data. Make sure it isn't bad data before using it.
-    Second Step: Insert the new data into the table 
-    */
+*/
+
+
